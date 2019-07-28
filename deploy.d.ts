2f7aeb8209b360e6c5490636ca871bb0247ec861
David Sandor <david.sandor@gmail.com>
2019-07-28 15:11:50 -0400

    checkpoint

---
+++ deploy.d.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import cdk = require('@aws-cdk/core');
+import { StaticWebsiteStack } from 'aws-cdk-static-website';
+export declare class MyWebStack extends StaticWebsiteStack {
+    constructor(scope: cdk.App, id: string);
+}
--- deploy.ts
+++ deploy.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 /*
 TODO: Make this take all the parameters from the package.json.
 */
@@ -7,15 +8,30 @@ import { StaticWebsiteStack, IStaticWebsiteProps } from 'aws-cdk-static-website'
 export class MyWebStack extends StaticWebsiteStack {
   constructor(scope: cdk.App, id: string ) {
     const props: IStaticWebsiteProps = {
-      websiteDistPath: './dist',
-      deploymentVersion: '1.0.0',
-      resourcePrefix: 'my-web-stack',
-      indexDocument: 'index.html',
+      websiteDistPath: pkg.webdeploy.distPath,
+      deploymentVersion: pkg.version,
+      resourcePrefix: pkg.webdeploy.resourcePrefix,
+      indexDocument: pkg.webdeploy.indexDocument,
     };
 
     super(scope, id, props);
   }
 }
 
+if (process.argv.length !== 3) {
+  console.error('FAIL, please pass current working directory to command. E.g. "aws-webdeploy $(pwd)" in "scripts"');
+  process.exit(1);
+}
+
+const workingPath = process.argv[2];
+const pkg = require(`${workingPath}/package.json`);
+
+console.log('pkg:', pkg);
+
+if (!pkg.webdeploy) {
+  console.error('Error, package.json should have a "webdeploy" configuration section.');
+  process.exit(2);
+}
+
 const app = new cdk.App();
-new MyWebStack(app, 'MyWebStack');
+new MyWebStack(app, pkg.webdeploy.stackName || pkg.name);
